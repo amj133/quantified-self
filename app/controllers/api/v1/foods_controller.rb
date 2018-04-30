e9faa9bf40c8dc962196b743ce7b61c9cf66a819
amj133 <amj@vt.edu>
2018-04-30 15:51:03 -0600

    add create action to foods controller and render 400 if food not saved

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -8,4 +8,14 @@ class Api::V1::FoodsController < ApplicationController
     render json: Food.find(params[:id])
   end
 
+  def create
+    food = Food.new(name: params["food"]["name"],
+                    calories: params["food"]["calories"])
+    if food.save
+      render json: food
+    else
+      render status: 400, json: {message: "Must provide name and calories for new food"}
+    end
+  end
+
 end
