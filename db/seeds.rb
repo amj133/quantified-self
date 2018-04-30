05cfe9fa2c8682eb6ff020cd9a2dde9eafa26af2
amj133 <amj@vt.edu>
2018-04-30 14:50:35 -0600

    add 50 foods to seed file with times loop

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do |i|
+  Food.create(name: "Name-#{i}",
+              calories: 100 + i)
+end
