154ba441b4f4f3c7ce72c3b6e11156d3a93f3da1
amj133 <amj@vt.edu>
2018-04-30 14:04:36 -0600

    write user can access all foods spec

--- spec/requests/api/v1/foods/user_can_access_all_foods_spec.rb
+++ spec/requests/api/v1/foods/user_can_access_all_foods_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 describe "foods finder api" do
-  it "can access all foods" do
-    get "/api/v1/foods"
+  context "GET /api/v1/foods" do
+    it "provides access to all foods" do
+      get "/api/v1/foods"
 
-    foods = JSON.parse(response.body)
+      foods = JSON.parse(response.body)
 
-    expect(response).to be_success
-    expect(foods.first[:id]).to eq(1)
-    expect(foods.last[:id]).to eq(2)
+      expect(response).to be_success
+      expect(foods.first[:id]).to eq(1)
+      expect(foods.last[:id]).to eq(2)
+    end
   end
 end
